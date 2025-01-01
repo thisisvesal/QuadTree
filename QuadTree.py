178824f477f694f46bec77192ce0917f9f26e263
thisisvesal <vesal.ea84@gmail.com>
2025-01-01 18:57:26 +0330

    Pixel depth

--- QuadTree.py
+++ QuadTree.py
@@ -38,8 +38,24 @@ class QuadTree:
         
         return max(self.topLeft.treeDepth(), self.topRight.treeDepth(), self.bottomLeft.treeDepth(), self.bottomRight.treeDepth()) + 1
 
-    def pixelDepth(self, px, py) -> int:
-        ...
+    def pixelDepth(self, px: int, py: int) -> int:
+        if not self.image:
+            return 0
+        if self.isLeaf():
+            return 1
+    
+        half_len = self.len // 2
+        if px < half_len and py < half_len:
+            return self.topLeft.pixelDepth(px, py) + 1
+        elif px >= half_len and py < half_len:
+            return self.topRight.pixelDepth(px - half_len, py) + 1
+        elif px < half_len and py >= half_len:
+            return self.bottomLeft.pixelDepth(px, py - half_len) + 1
+        elif px >= half_len and py >= half_len:
+            return self.bottomRight.pixelDepth(px - half_len, py - half_len) + 1
+    
+        return 0
+
 
     def searchSubspaceWithRange(self, x1, y1, x2, y2) -> None:
         ...
